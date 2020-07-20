b5f21088cd4a70ba3d0b6d080066aaafb1094efb
mikaelbdj <201805344@post.au.dk>
2020-07-20 18:20:26 +0200

    Add generation of row rule clauses by TDD

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -3,6 +3,9 @@ import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SudokuSolver {
 
 
@@ -28,28 +31,63 @@ public class SudokuSolver {
     }
 
     //sudoku rules as clauses
-    private static Vec<IVecInt> generateRuleClauses () {
+    private static Vec<IVecInt> generateRuleClauses() {
         Vec<IVecInt> ruleClauses = new Vec<>();
         //row rules
+        List<IVecInt> rowRules = generateRowRuleClauses();
+        for (IVecInt rowRule : rowRules) {
+            ruleClauses.push(rowRule);
+        }
 
         //column rules
 
         //box rules
 
-        //one number per cell constraint
+        //cell rules
 
         return ruleClauses;
     }
 
+    private static List<IVecInt> generateRowRuleClauses() {
+        List<IVecInt> rowRules = new ArrayList<>();
+
+        //definedness
+        for (int row = 0; row < 9; row++) {
+            for (int value = 1; value <= 9; value++) {
+                int[] definedness = new int[9];
+                for (int col = 0; col < 9; col++) {
+                    definedness[col] = getLiteralFromCell(row, col, value);
+                }
+                rowRules.add(new VecInt(definedness));
+            }
+        }
+
+        //uniqueness
+        for (int row = 0; row < 9; row++) {
+            for (int value = 1; value <= 9; value++) {
+                for (int col1 = 0; col1 < 8; col1++) {
+                    for (int col2 = col1 + 1; col2 < 9; col2++) {
+                        int[] uniqueness = new int[2];
+                        uniqueness[0] = -getLiteralFromCell(row, col1, value);
+                        uniqueness[1] = -getLiteralFromCell(row, col2, value);
+                        rowRules.add(new VecInt(uniqueness));
+                    }
+                }
+            }
+        }
+
+        return rowRules;
+    }
+
 
     //clauses from the given numbers
-    private static Vec<IVecInt> generateNumberClauses (int[][] sudoku) {
+    private static Vec<IVecInt> generateNumberClauses(int[][] sudoku) {
         Vec<IVecInt> numberClauses = new Vec<>();
 
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 int value = sudoku[row][col];
-                int literal = (row * 9 * 9) + (col * 9) + value;
+                int literal = getLiteralFromCell(row, col, value);
                 if (value != 0) {
                     numberClauses.push(new VecInt(new int[]{literal}));
                 }
@@ -57,5 +95,9 @@ public class SudokuSolver {
         }
 
         return numberClauses;
+    }
+
+    private static int getLiteralFromCell(int row, int col, int value) {
+        return (row * 9 * 9) + (col * 9) + value;
     }
 }
--- src/test/java/TestSudokuSolver.java
+++ src/test/java/TestSudokuSolver.java
@@ -7,6 +7,8 @@ import org.sat4j.specs.IVecInt;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 
@@ -68,5 +70,49 @@ public class TestSudokuSolver {
         Assert.assertThat(clauses.get(1), is(new VecInt(new int[]{115})));
         Assert.assertThat(clauses.get(2), is(new VecInt(new int[]{518})));
         Assert.assertThat(clauses.get(3), is(new VecInt(new int[]{729})));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testCorrectDefinednessRowRuleClauses () throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = SudokuSolver.class.getDeclaredMethod("generateRowRuleClauses");
+        method.setAccessible(true);
+
+        List<IVecInt> clauses = (List<IVecInt>) method.invoke(null);
+
+        IVecInt definednessClause0 = new VecInt(new int[]{1,10,19,28,37,46,55,64,73}); //at least one value of 1 in row 0
+        IVecInt definednessClause1 = new VecInt(new int[]{86,95,104,113,122,131,140,149,158}); //at least one value of 5 in row 1
+
+        System.out.println(clauses.get(8));
+        Assert.assertThat(clauses.size(), is(2997));
+        Assert.assertThat(clauses.contains(definednessClause0), is(true));
+        Assert.assertThat(clauses.contains(definednessClause1), is(true));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testCorrectUniquenessRowRuleClauses () throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = SudokuSolver.class.getDeclaredMethod("generateRowRuleClauses");
+        method.setAccessible(true);
+
+        List<IVecInt> clauses = (List<IVecInt>) method.invoke(null);
+
+        int[][] uniquenessClausesArray = new int[][] {
+                {-1,-10},{-1,-19},{-1,-28},{-1,-37},{-1,-46},{-1,-55},{-1,-64},{-1,-73},
+                {-10,-19},{-10,-28},{-10,-37},{-10,-46},{-10,-55},{-10,-64},{-10,-73},
+                {-19,-28},{-19,-37},{-19,-46},{-19,-55},{-19,-64},{-19,-73},
+                {-28,-37},{-28,-46},{-28,-55},{-28,-64},{-28,-73},
+                {-37,-46},{-37,-55},{-37,-64},{-37,-73},
+                {-46,-55},{-46,-64},{-46,-73},
+                {-55,-64},{-55,-73},
+                {-64,-73},
+        }; //at most one value of 1 in row 0
+        List<IVecInt> uniquenessClauses = new ArrayList<>();
+        for (int[] uniquenessClause : uniquenessClausesArray) {
+            uniquenessClauses.add(new VecInt(uniquenessClause));
+        }
+
+        Assert.assertThat(clauses.size(), is(2997));
+        Assert.assertThat(clauses.containsAll(uniquenessClauses), is(true));
     }
 }
