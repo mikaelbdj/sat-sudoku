44e8fc6d0051a3c4a7b6d00dd76add2099ebc54f
mikaelbdj <201805344@post.au.dk>
2020-07-22 00:54:38 +0200

    Make sudoku printing in terminal prettier

--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,7 +15,7 @@ public class Sudoku {
         this.sudoku = sudoku;
         this.size = sudoku.length;
 
-        if (size != sudoku[0].length) throw new SudokuException("Height and length of sudoku does not match");
+        if (Arrays.stream(sudoku).anyMatch(row -> row.length != size)) throw new SudokuException("Height and length of sudoku does not match");
         if (Math.pow(Math.sqrt(size),2) != size) throw new SudokuException("Sudoku size is not a perfect square");
         if (flatValues().stream().anyMatch(value -> size < value || value < 0)) throw new SudokuException("Sudoku contains too large or negative values");
     }
@@ -27,17 +28,30 @@ public class Sudoku {
         return size;
     }
 
+    //really fancy string representation
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (int[] row : sudoku) {
-            for (int cell : row) {
-                stringBuilder.append(cell).append(" ");
+        int boxSize = (int) Math.sqrt(size);
+        int lineLength = size * 3 + boxSize * 2 + 1 - (size <= 9 ? size : 0);
+        stringBuilder.append("-".repeat(lineLength));
+        stringBuilder.append("\n");
+
+        for (int row = 0; row < size; row++) {
+            stringBuilder.append("| ");
+            for (int col = 0; col < size; col++) {
+                int value  = getCell(row, col);
+                boolean boxLine = ((col + 1) % boxSize) == 0;
+                stringBuilder.append(value).append(size <= 9 || 9 < value ? " " : "  ");
+                stringBuilder.append(boxLine ? "| " : "");
             }
             stringBuilder.append("\n");
+            if ((row +1 ) % boxSize == 0) {
+                stringBuilder.append("-".repeat(lineLength));
+                stringBuilder.append("\n");
+            }
         }
-
         return stringBuilder.toString();
     }
 
